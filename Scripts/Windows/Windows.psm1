b2f019e04022c73d4062fe26bd1464f155c9159f
KURIYAMA Takatoshi <takatoshi.kuriyama@gmail.com>
2024-05-25 20:19:44 +0900

    Update Scripts/Windows/Windows.psm1

--- Scripts/Windows/Windows.psm1
+++ Scripts/Windows/Windows.psm1
@@ -19,3 +19,46 @@ function Edit-Hosts {
 
 #     Stop-Process -Name explorer -Force
 # }
+
+if (Get-Command -Name docker -ErrorAction SilentlyContinue) {
+    # NOTE: This function depends Windows Subsystem for Linux.
+    function Optimize-DockerUsage {
+        # NOTE: Requires running as Administrator.
+        [CmdletBinding()]
+        param (
+            [Parameter()]
+            [switch]
+            $ForcePrune
+        )
+        $ack = Read-Host 'Do you want to optimize docker usage? [y/n]'
+        if ($ack -eq 'y') {
+            Write-Host 'acknowledged.'
+        }
+        else {
+            Write-Host 'canceled.'
+            return
+        }
+        if ($ForcePrune) {
+            if (Get-Process 'com.docker.backend' -ErrorAction SilentlyContinue) {
+                Write-Host 'docker backend is running. prune all containers, images, and volumes.'
+                docker system prune --all --force
+                Write-Host 'pruned.'
+            }
+            else {
+                Write-Host 'docker backend is not running. skip pruning.'
+            }
+        }
+        Write-Host 'shutdown wsl.'
+        wsl --shutdown
+        Write-Host 'compact vhdx.'
+        $vdisk = Resolve-Path "${env:LOCALAPPDATA}\Docker\wsl\data\ext4.vhdx"
+        $tmp = "${env:Temp}/diskpart.txt"
+        @"
+select vdisk file="$vdisk"
+compact vdisk
+"@ | Set-Content -Path $tmp
+        diskpart /s $tmp > ./log.txt
+        Get-Content ./log.txt | Write-Host
+        Remove-Item $tmp, ./log.txt
+    }
+}
