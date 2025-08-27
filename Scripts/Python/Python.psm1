2b5a4e366427e86888b86d377fc66be3cc5171e3
KURIYAMA Takatoshi <takatoshi.kuriyama@gmail.com>
2025-08-27 09:45:02 +0900

    Update Scripts/Python/Python.psm1

--- Scripts/Python/Python.psm1
+++ Scripts/Python/Python.psm1
@@ -1,19 +1,23 @@
+function Resolve-UvPythonPath {
+    # NOTE: uv doesn't append the path for dll. So, I need to do it manually.
+    Resolve-Path "$($env:APPDATA)/uv/python/*/python*.dll" | Split-Path -Parent | Get-Unique | Sort-Object -Descending | Select-Object -First 1 | ForEach-Object { $env:Path += ';' + $_ }
+}
+
 function Update-PipModules {
-    if (-not (Get-Command pyenv -ErrorAction SilentlyContinue)) {
-        Write-Error "Install pyenv with command below. 'choco install pyenv-win -y'"
+    if (-not (Get-Command uv -ErrorAction SilentlyContinue)) {
+        Write-Error "Install uv with command below. 'choco install -y uv'"
         return
     }
     $firstTime = -not (Get-Command pip -ErrorAction SilentlyContinue)
     if ($firstTime) {
-        $latest = pyenv install -l | Where-Object -FilterScript { -not ($_ -match '[a-zA-Z]') } | Select-Object -Last 1
-        pyenv install $latest
-        pyenv global $latest
+        $latest = '3.13'
+        uv python install $latest
+        Resolve-UvPythonPath
     }
     python -m pip install --upgrade pip
     if ($firstTime) {
         $list = @(
-            'poetry'
-            'openai'
+            'sqlfmt'
         )
         pip install ($list -join ' ')
     }
@@ -25,13 +29,6 @@ function Update-PipModules {
     }
 }
 
-function Remove-CurrentVirtualenv {
-    if (Test-Path pyproject.toml) {
-        poetry env list | Where-Object { $_ -like "*$(Get-Location | Split-Path -Leaf)*" } | Select-Object -First 1 | ForEach-Object { ($_ -split ' ')[0] }
-    }
-}
-
-
 if (Get-Command -Name uv -ErrorAction SilentlyContinue) {
     try {
         uv generate-shell-completion powershell 2>&1 | Out-String | Invoke-Expression
@@ -39,8 +36,7 @@ if (Get-Command -Name uv -ErrorAction SilentlyContinue) {
     catch {
         Write-Warning "uv completions --shell power-shell failed. $($_)"
     }
-    # NOTE: uv doesn't append the path for dll. So, I need to do it manually.
-    Resolve-Path "$($env:APPDATA)/uv/python/*/python*.dll" | Split-Path -Parent | Get-Unique | ForEach-Object { $env:Path += ';' + $_ }
+    Resolve-UvPythonPath
 }
 else {
     Write-Warning "Install uv with command below. 'choco install uv -y'"
