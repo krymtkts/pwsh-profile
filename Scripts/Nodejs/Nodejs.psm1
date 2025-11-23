927a60e2000f610e6bbb62ce6e5594f965e92cca
KURIYAMA Takatoshi <takatoshi.kuriyama@gmail.com>
2025-11-23 21:26:09 +0900

    Update Scripts/Nodejs/Nodejs.psm1

--- Scripts/Nodejs/Nodejs.psm1
+++ Scripts/Nodejs/Nodejs.psm1
@@ -1,3 +1,8 @@
+if (-not (Get-Command fnm -ErrorAction SilentlyContinue)) {
+    Write-Error "Install fnm with command below. 'choco install fnm -y'"
+    return
+}
+
 function Install-NodeModules {
     # NOTE: workaround for certificate issue.
     $env:NODE_TLS_REJECT_UNAUTHORIZED = 0
@@ -6,10 +11,6 @@ function Install-NodeModules {
     $env:NODE_TLS_REJECT_UNAUTHORIZED = 1
 }
 function Update-NodeModules {
-    if (-not (Get-Command fnm -ErrorAction SilentlyContinue)) {
-        Write-Error "Install fnm with command below. 'choco install fnm -y'"
-        return
-    }
     $firstTime = -not (Get-Command npm -ErrorAction SilentlyContinue)
     if ($firstTime) {
         20, 22 | ForEach-Object { fnm install "v$_" }
@@ -37,50 +38,48 @@ function Update-NodeModules {
     }
 }
 
-if (Get-Command -Name fnm -ErrorAction SilentlyContinue) {
-    fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
-    try {
-        fnm completions --shell powershell 2>&1 | Out-String | Invoke-Expression
-    }
-    catch {
-        Write-Warning "fnm completions --shell powershell failed. $($_)"
-    }
-    Get-ChildItem "$env:FNM_MULTISHELL_PATH/../" | Where-Object -Property CreationTime -LE (Get-Date).AddDays(-1) | Remove-Item
+fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+try {
+    fnm completions --shell powershell 2>&1 | Out-String | Invoke-Expression
+}
+catch {
+    Write-Warning "fnm completions --shell powershell failed. $($_)"
+}
+Get-ChildItem "$env:FNM_MULTISHELL_PATH/../" | Where-Object -Property CreationTime -LE (Get-Date).AddDays(-1) | Remove-Item
 
-    Register-ArgumentCompleter -Native -CommandName 'npm' -ScriptBlock {
-        param($wordToComplete, $commandAst, $cursorPosition)
+Register-ArgumentCompleter -Native -CommandName 'npm' -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
 
-        $commandAst = "$commandAst".Substring(0, $cursorPosition - $wordToComplete.Length)
-        if ($commandAst -match 'npm\s*$') {
-            $help = npm --help
-            $help = $help[($help.IndexOf('All commands:') + 2)..($help.Count - 1)]
-            $help[0..($help.IndexOf('') - 1)] -join '' -split ',' `
-            | ForEach-Object { $_.Trim() } `
-            | Where-Object { $_ -like "${wordToComplete}*" } `
-            | ForEach-Object {
-                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-            }
-        }
-        if (-not (Test-Path ./package.json)) {
-            return
-        }
-        if ($commandAst -match 'npm run(-script)?\s*$') {
-            Get-Content .\package.json `
-            | ConvertFrom-Json `
-            | Select-Object -ExpandProperty scripts `
-            | Get-Member -MemberType NoteProperty `
-            | Where-Object -Property Name -Like "${wordToComplete}*" `
-            | ForEach-Object {
-                [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Definition)
-            }
+    $commandAst = "$commandAst".Substring(0, $cursorPosition - $wordToComplete.Length)
+    if ($commandAst -match 'npm\s*$') {
+        $help = npm --help
+        $help = $help[($help.IndexOf('All commands:') + 2)..($help.Count - 1)]
+        $help[0..($help.IndexOf('') - 1)] -join '' -split ',' `
+        | ForEach-Object { $_.Trim() } `
+        | Where-Object { $_ -like "${wordToComplete}*" } `
+        | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
         }
     }
-
-    function Set-CurrentNodeVersionToSystem {
-        $defaultNodeJs = Join-Path $env:APPDATA 'fnm' 'node-versions' $(fnm current) 'installation'
-        $userPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::User)
-        if (-not ($userPath -contains $defaultNodeJs)) {
-            [System.Environment]::SetEnvironmentVariable('PATH', "$defaultNodeJs;$($userPath)", [System.EnvironmentVariableTarget]::User)
+    if (-not (Test-Path ./package.json)) {
+        return
+    }
+    if ($commandAst -match 'npm run(-script)?\s*$') {
+        Get-Content .\package.json `
+        | ConvertFrom-Json `
+        | Select-Object -ExpandProperty scripts `
+        | Get-Member -MemberType NoteProperty `
+        | Where-Object -Property Name -Like "${wordToComplete}*" `
+        | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Definition)
         }
+    }
+}
+
+function Set-CurrentNodeVersionToSystem {
+    $defaultNodeJs = Join-Path $env:APPDATA 'fnm' 'node-versions' $(fnm current) 'installation'
+    $userPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::User)
+    if (-not ($userPath -contains $defaultNodeJs)) {
+        [System.Environment]::SetEnvironmentVariable('PATH', "$defaultNodeJs;$($userPath)", [System.EnvironmentVariableTarget]::User)
     }
 }
